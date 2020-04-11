72b971d35f03e3a02a2c8e86741a9dd7527d50af
Aklarafkaf <Kiliop@outlook.de>
2020-04-11 20:26:09 +0200

    Erwiterung hinzugefugt

---
+++ data/IA-c.ps1
@@ -0,0 +1,62 @@
+﻿function Load {
+param (
+[parameter(Mandatory=$true)][float]$weit
+)
+$sleept = [float]$weit / 20
+clear
+echo "[█___________________]"
+sleep -Milliseconds $sleept
+clear
+echo "[██__________________]"
+sleep -Milliseconds $sleept
+clear
+echo "[███_________________]"
+sleep -Milliseconds $sleept
+clear
+echo "[████________________]"
+sleep -Milliseconds $sleept
+clear
+echo "[█████_______________]"
+sleep -Milliseconds $sleept
+clear
+echo "[██████______________]"
+sleep -Milliseconds $sleept
+clear
+echo "[███████_____________]"
+sleep -Milliseconds $sleept
+clear
+echo "[████████____________]"
+sleep -Milliseconds $sleept
+clear
+echo "[█████████___________]"
+sleep -Milliseconds $sleept
+clear
+echo "[██████████__________]"
+sleep -Milliseconds $sleept
+clear
+echo "[███████████_________]"
+sleep -Milliseconds $sleept
+clear
+echo "[██████████████______]"
+sleep -Milliseconds $sleept
+clear
+echo "[███████████████______]"
+sleep -Milliseconds $sleept
+clear
+echo "[████████████████____]"
+sleep -Milliseconds $sleept
+clear
+echo "[█████████████████___]"
+sleep -Milliseconds $sleept
+clear
+echo "[██████████████████__]"
+sleep -Milliseconds $sleept
+clear
+echo "[██████████████████__]"
+sleep -Milliseconds $sleept
+clear
+echo "[███████████████████_]"
+sleep -Milliseconds $sleept
+clear
+echo "[████████████████████]"
+}
